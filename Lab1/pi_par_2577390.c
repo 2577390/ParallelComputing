9d27eac579a730ae99c75ca4ea1628a49a79a91d
Lebo Kharafu <Kharafu23@gmail.com>
2025-03-18 20:59:44 +0200

    Fixed it all and added formatting

--- Lab1/pi_par_2577390.c
+++ Lab1/pi_par_2577390.c
@@ -1,15 +1,95 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <omp.h>
-#define NUM_THREADS 2
+#define NUM_THREADS 12
 
-/*
-long num_steps = 1000000;
-double step = 1.0 / (double) num_steps;
-*/
-double serialPi(){
+void usage(char prog_name[]);
+double serialPi(double step);
+double falseSharingPi(double step);
+double raceConditionPi(double step);
+double noRaceConditionPi(double step);
+static long num_steps = 1000000;
 
-    long num_steps = 1000000;
-    double step = 1.0 / (double) num_steps;
+int main(int argc, char const *argv[])
+{
+    double start_time, run_time=0, pi, step, tSerial,tParallel,speedup;
+	int iter;
+
+
+	if (argc != 2) {
+		usage(argv[0]);
+		exit (-1);
+	}
+	
+    run_time=0;
+    iter=atoi(argv[1]);
+	step = 1.0/(double)num_steps;
+	for(int i=0; i<iter; i++){
+        // Record the time for staring the computation of pi 
+		start_time = omp_get_wtime();
+		pi=serialPi(step);
+        // Record the time for ending of the computation of pi and
+		//find the elapsed time for the computation of pi
+		//Note we are running the same process for multiple time and taking the average here 
+		run_time += omp_get_wtime() - start_time;
+	}
+    tSerial = run_time/iter;
+	printf("\n Sequential: pi with %ld steps is %f in %f seconds\n",num_steps,pi,tSerial);
+    
+    run_time=0;
+    iter=atoi(argv[1]);
+	step = 1.0/(double)num_steps;
+    for(int i=0; i<iter; i++){
+        // Record the time for staring the computation of pi 
+		start_time = omp_get_wtime();
+		pi=falseSharingPi(step);
+        // Record the time for ending of the computation of pi and
+		//find the elapsed time for the computation of pi
+		//Note we are running the same process for multiple time and taking the average here 
+		run_time += omp_get_wtime() - start_time;
+	}
+    tParallel = run_time/iter;
+    speedup = tSerial / tParallel;
+	printf("\n Parallel with false sharing: pi with %ld steps is %f in %f seconds using %d threads\n",num_steps,pi,tParallel,NUM_THREADS);
+    printf("Speedup: %f\n",speedup);
+    
+    run_time=0;
+    iter=atoi(argv[1]);
+	step = 1.0/(double)num_steps;
+    for(int i=0; i<iter; i++){
+        // Record the time for staring the computation of pi 
+		start_time = omp_get_wtime();
+		pi=raceConditionPi(step);
+        // Record the time for ending of the computation of pi and
+		//find the elapsed time for the computation of pi
+		//Note we are running the same process for multiple time and taking the average here 
+		run_time += omp_get_wtime() - start_time;
+	}
+    tParallel =run_time/iter;
+    speedup = tSerial / tParallel;
+	printf("\n Parallel with race Condtion: pi with %ld steps is %f in %f seconds using %d threads\n",num_steps,pi,tParallel,NUM_THREADS);
+    printf("Speedup: %f\n",speedup);
+
+    run_time=0;
+    iter=atoi(argv[1]);
+	step = 1.0/(double)num_steps;
+    for(int i=0; i<iter; i++){
+        // Record the time for staring the computation of pi 
+		start_time = omp_get_wtime();
+		pi=noRaceConditionPi(step);
+        // Record the time for ending of the computation of pi and
+		//find the elapsed time for the computation of pi
+		//Note we are running the same process for multiple time and taking the average here 
+		run_time += omp_get_wtime() - start_time;
+	}
+    tParallel =run_time/iter;
+    speedup = tSerial / tParallel;
+	printf("\n Parallel with no race Condtion: pi with %ld steps is %f in %f seconds using %d threads\n",num_steps,pi,tParallel,NUM_THREADS);
+    printf("Speedup: %f\n",speedup);
+    return EXIT_SUCCESS; 
+}
+
+double serialPi(double step){
     double x, pi, sum = 0.0;
 
     for(int  i = 0; i < num_steps; i++){
@@ -18,17 +98,10 @@ double serialPi(){
     }
 
     pi = step * sum;
-    printf("Seqential: pi with %d steps is %f  in () seconds using () %d threads\n Speedup: ()\n ",pi);
-
     return pi;
 }
 
-
-double falseSharingPi(){
-
-
-    long num_steps = 1000000;
-    double step = 1.0 / (double) num_steps;
+double falseSharingPi(double step){
 
     int nthreads;
     double pi;
@@ -37,7 +110,7 @@ double falseSharingPi(){
     omp_set_num_threads(NUM_THREADS);
     #pragma omp parallel
     {
-        int id, tthreads; 
+        int i,id, tthreads; 
         double x;
 
         tthreads = omp_get_num_threads();
@@ -45,68 +118,55 @@ double falseSharingPi(){
         if(id == 0){
             nthreads = tthreads;
         }
-        sum[id] = 0;
-        for(int i = id; i < num_steps; i += tthreads){
+        
+        for(i = id,sum[id] = 0.0; i < num_steps; i += tthreads){
             x = (i + 0.5 ) * step;
             sum[id] += 4.0 / (1 + (x*x));
         }
     }
-
+    pi = 0.0;
     for (int i = 0; i < nthreads; i++)
     {
         /* code */
         pi += sum[i] * step;
     }
     
-    
-    printf("Parallel with false sharing: pi with %d steps is %f  in () seconds using () %d threads\n Speedup: ()\n ",pi);
     return pi;
 }
 
+double raceConditionPi(double step){
 
-double raceConditionPi(){
-
-
-    long num_steps = 1000000;
-    double step = 1.0 / (double) num_steps;
     
     double pi = 0.0;
-    double sum = 0.0;
-    double x;
-
-    int id, tthreads; 
-    
+    double sum = 0.0;    
 
     #pragma omp parallel
     {  
+        int id, tthreads; 
+        double x;
         tthreads = omp_get_num_threads();
         id = omp_get_thread_num();
         for(int i = id; i < num_steps; i += tthreads){
             x = (i + 0.5 ) * step;
             sum += 4.0 / (1.0 + (x*x));
         }
+
+        pi += step * sum;   
     }
 
-    pi += step * sum;    
+     
     
-    printf("Method 2 = %f \n",pi);
     return pi;
 }
 
-
-double noRaceConditionPi(){
-
-    long num_steps = 1000000;
-    double step = 1.0 / (double) num_steps;
+double noRaceConditionPi(double step){
     
     double pi = 0.0;
-    
-
-    int id, tthreads; 
-    double x;
 
     #pragma omp parallel
     {  
+        int id, tthreads; 
+        double x;
         tthreads = omp_get_num_threads();
         id = omp_get_thread_num();
         double sum = 0.0;
@@ -118,22 +178,17 @@ double noRaceConditionPi(){
         #pragma omp atomic
         pi += step * sum;    
 
-    }
-
-     
+    } 
     
-    printf("Method 3 = %f \n",pi);
     return pi;
 }
 
-int main(int argc, char const *argv[])
-{
-    /* code */
-
-    serialPi();
-    falseSharingPi();
-    raceConditionPi();
-    noRaceConditionPi();
-    return 0;
-}
-
+/*--------------------------------------------------------------------
+ * Function:    usage
+ * Purpose:     Print command line for function
+ * In arg:      prog_name
+ */
+void usage(char prog_name[]) {
+    fprintf(stderr, "usage:  %s <number of times to run>\n", prog_name);
+ } /* usage */
+ 
---
+++ Lab1/run.sh
@@ -0,0 +1,4 @@
+echo ---------pi_par_2577390 is started----------
+./pi_par_2577390 |& tee -a terminal_output.out
+echo ---------hello_omp is done-------------
+echo
